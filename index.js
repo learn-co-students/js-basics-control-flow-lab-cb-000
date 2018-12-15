0196abc75178a1b22368c296f1264c74683ae3cf
Elizabeth J <liz.jenerson@gmail.com>
2018-12-14 21:05:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(value) {
+
+	if (value <= 400) {
+		return 'This one is on me!';
+	}
+	else if (value > 2000 && value < 2500) {
+		return 'I will gladly take your thirty bucks.';
+	}
+	else {
+		return 'No can do.';
+	}
+}
+
+function ternaryCheckCity(value){
+	return value === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(howGenerous) {
+	switch(howGenerous){
+		case 'generous':
+			return "Thank you so much.";
+			break;
+
+		case 'not as generous':
+			return "Thank you.";
+			break;
+
+		default:
+			return "Bye.";
+	}
+}
