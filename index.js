095c64fbc0443b092df79a6b6ecab97577459cf8
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-18 16:27:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist) {
+  if (dist < 200) {
+    return 'This one is on me!'
+  }
+  else if (dist > 2000 && dist <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (dist > 2500) {
+    return 'No can do.'
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default :
+      return "Bye."
+  }
 }
